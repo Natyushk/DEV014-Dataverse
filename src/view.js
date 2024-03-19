e7e4b4dc17a1b72943b1b8af79528c9b4db7ad36
Natyushk <136663795+Natyushk@users.noreply.github.com>
2024-03-19 17:55:39 -0500

    Cambios en la logica de view.js

--- src/view.js
+++ src/view.js
@@ -1,15 +1,30 @@
-export const renderItems = (data) => {
-  let newUl = document.createElement("ul");
+// Definición de la función renderItems corregida
+export const renderItems = (dataArray) => {
+  // Verificar si dataArray es un array
+  if (!Array.isArray(dataArray)) {
+    console.error("Data debe ser un array.");
+    return;
+  }
+
+  // Crear un nuevo ul
+  const newUl = document.createElement("ul");
   document.body.appendChild(newUl);
-  data.forEach(element => {
-    let newLi = document.createElement("li");
+
+  // Iterar sobre cada elemento en el array
+  dataArray.forEach(element => {
+    // Crear un nuevo li para cada elemento
+    const newLi = document.createElement("li");
     newUl.appendChild(newLi);
-    let nodoImg = document.createElement("img");
-    nodoImg.setAttribute("src", element.imageUrl)
+
+    // Crear y configurar la imagen
+    const nodoImg = document.createElement("img");
+    nodoImg.src = element.imageUrl;
     newLi.appendChild(nodoImg);
-    let newP = document.createElement("p");
+
+    // Crear y configurar el párrafo con el nombre
+    const newP = document.createElement("p");
+    newP.textContent = element.name;
     newLi.appendChild(newP);
-    newP.innerHTML = element.name;
+    newP.innerHTML = element.name
   });
-  return nombre;
 };
