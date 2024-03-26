a0859262b8b168955f1624d9aa7d0ec05aeb49ac
sandymmora <sandy.mmora@gmail.com>
2024-03-26 16:42:57 -0600

    Se agrego el boton limpiar y su funcionalidad

--- src/main.js
+++ src/main.js
@@ -3,52 +3,46 @@ import data from './data/dataset.js';
 import { renderItems } from './view.js';
 
 // Importar funciones
-import { filterByContinent, sortBy, calculateFortuneStats } from './dataFunctions.js';
-
-// import { filterByContinent, sortBy, calculateTotalFortune } from './dataFunctions.js';
-// Renderizar los elementos inicilmente
-
-const richPeopleList = document.getElementById('rich-people-list');
-const totalFortuneElement = document.getElementById('total-fortune');
-const averageFortuneElement = document.getElementById('average-fortune');
-
-//Renderizar los elementos inicialmente
-richPeopleList.appendChild(renderItems(data));
-
+import { filterByContinent, sortBy, calculateTotalFortune, calculateFortuneStats } from './dataFunctions.js';
 
 // Referencias a elementos del DOM
 const continentFilterSelect = document.getElementById('continent-filter');
 const sortBySelect = document.getElementById('sort-by');
+const richPeopleList = document.getElementById('rich-people-list');
+const totalFortuneElement = document.getElementById('total-fortune');
+const averageFortuneElement = document.getElementById('average-fortune');
+const resetButton = document.getElementById('reset-button');
 
-//const richPeopleList = document.getElementById('rich-people-list');
+// Renderizar los elementos inicilmente
+richPeopleList.appendChild(renderItems(data));
 
 // Manejador de evento para cambio en select
 continentFilterSelect.addEventListener('change', () => {
   const continent = continentFilterSelect.value;
   const filteredData = filterByContinent(data, continent);
-  const sortedData = sortBy(filteredData, 'fortune', 'asc');
   richPeopleList.innerHTML = '';
-  richPeopleList.appendChild(renderItems(sortedData));
-  updateFortuneStats(sortedData);
+  richPeopleList.appendChild(renderItems(filteredData));
+  updateFortuneStats(filteredData);
 });
 
 // Manejador de evento para el botón de ordenar
 sortBySelect.addEventListener('change', () => {
   const [field, order] = sortBySelect.value.split('-');
   const sortedData = sortBy(filterByContinent(data,continentFilterSelect.value), field, order);
-  richPeopleList.remove();
-  richPeopleList = document.querySelector("main").appendChild(renderItems(sortedData));
+  richPeopleList.innerHTML = '';
+  richPeopleList.appendChild(renderItems(sortedData));
   updateTotalFortune(sortedData);
 });
 
-Función para actualizar y mostrar la fortuna total
+//Función para actualizar y mostrar la fortuna total
 const updateTotalFortune = (data) => {
-  const totalFortune = calculateTotalFortune(data); totalFortuneElement.textContent = `Fortuna total: ${formatFortune(totalFortune)}`;
-// };
+  const totalFortune = calculateTotalFortune(data); 
+  totalFortuneElement.textContent = `Fortuna total: ${formatFortune(totalFortune)}`;
+  // };
   const sortedData = sortBy(data, field, order);
   richPeopleList.innerHTML = '';
   richPeopleList.appendChild(renderItems(sortedData));
-});
+};
 
 // Función para actualizar y mostrar la fortuna total y el promedio
 const updateFortuneStats = (data) => {
@@ -64,3 +58,10 @@ const formatFortune = (fortune) => {
 
 // Actualizar las estadísticas de fortuna inicialmente
 updateFortuneStats(data);
+
+//Función para boton limpiar
+resetButton.addEventListener('click', () => {
+  continentFilterSelect.value = 'Todos';
+  richPeopleList.innerHTML = '';
+  richPeopleList.appendChild(renderItems(data));
+});
